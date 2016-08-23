598398a2ab6af13404ebc3d75b0799bec03006ec
gopal.shah <gshah@starbucks.com>
2016-08-23 16:47:46 -0700

    Add custom encoder/decoder for List[B]

--- src/main/scala/persistence/AbcPersistence.scala
+++ src/main/scala/persistence/AbcPersistence.scala
@@ -2,13 +2,23 @@ package persistence
 
 import com.datastax.driver.core.Session
 import dto.A
+import dto.B
 import io.getquill._
+import java.util.List
 
 class AbcPersistence(private[persistence] val session: Session) {
   val ctx = new CassandraSyncContext[SnakeCase]("ctx")
 
   def foo(id: String) = {
     import ctx._
+    implicit val bDecoder: Decoder[List[B]] =
+      decoder[List[B]] {
+        row => index =>
+          row.getList(index, classOf[B])
+      }
+    implicit val bEncoder: Encoder[List[B]] =
+      encoder[List[B]](row => (idx, bList) =>
+        row.setList(idx, bList, classOf[B]))
 
     val q = quote {
       query[A].filter(_.id == lift(id))
